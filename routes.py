5538822e5aea48d55150da44d40c6983585eb15a
Ian W. Scott <iscott@tyndale.ca>
2012-10-01 17:32:46 -0400

    Fixes for standard blog list styles

--- routes.py
+++ routes.py
@@ -4,11 +4,17 @@
 # default_application, default_controller, default_function
 # are used when the respective element is missing from the
 # (possibly rewritten) incoming URL
-#
-default_controller = 'plugin_blog'
 
-# routes_in = (
-#     ('/articles/$anything', '/plugin_blog/articles/$anything'),
-#     ('/classes/$anything', '/plugin_blog/classes/$anything')
-# )
-# routes_out = [(x, y) for (y, x) in routes_in]
+routes_in = (
+    ('/index', '/blog/plugin_blog/index'),
+    ('/articles/$anything', '/blog/plugin_blog/articles/$anything'),
+    ('/classes/$anything', '/blog/plugin_blog/classes/$anything'),
+    ('/blog/default/download/$anything', '/blog/default/download/$anything'),
+    # make sure you do not break admin
+    ('/admin', '/admin'),
+    ('/admin/$anything', '/admin/$anything'),
+    # make sure you do not break appadmin
+    ('/blog/appadmin', '/blog/appadmin'),
+    ('/blog/appadmin/$anything', '/blog/appadmin/$anything'),
+)
+routes_out = [(x, y) for (y, x) in routes_in]
