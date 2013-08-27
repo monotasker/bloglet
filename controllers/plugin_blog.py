460e11bf583a20cb225bd0be82e494c4c02b9373
Ian Scott <scottianw@gmail.com>
2013-08-27 16:28:34 -0400

    Moved blog over to bootstrap-based framework; implemented plugin_listandedit; implemented topic browsing.

--- controllers/plugin_blog.py
+++ controllers/plugin_blog.py
@@ -1,18 +1,19 @@
 # coding: utf8
 
-from pprint import pprint
 if 0:
-    from gluon import current, TR, TD, TABLE, A, URL
+    from gluon import current
     from gluon.tools import DAL, Crud
     db = DAL()
     crud = Crud()
     request, session, db = current.request, current.session, current.db
     auth = current.auth
 
+import re
+
 
 def smart_truncate(content, length=480, suffix='...'):
     return (content if len(content) <= length
-                    else content[: length].rsplit(' ', 1)[0] + suffix)
+            else content[: length].rsplit(' ', 1)[0] + suffix)
 
 
 def index():
@@ -39,7 +40,7 @@ def articles():
 def classes():
     the_id = request.args[0]
     c = db((db.articles.id == the_id) &
-                 (db.articles.blog_tags.contains(5))).select().first()
+           (db.articles.blog_tags.contains(5))).select().first()
     tags = c.blog_tags
     alldocs = db(db.docs.id > 0).select()
     docs = alldocs.find(lambda row: row.id in c.docs)
@@ -51,57 +52,43 @@ def classes():
 
 
 @auth.requires(auth.user_id == 2)
-def all_posts():
-    posts = db().select(db.articles.ALL, orderby='created')
-    post_list = TABLE()
-    for p in posts:
-        postline = TR(TD(A(p.title, _href=URL('plugin_blog', 'edit_post',
-                      args=[p.id]))), TD(p.created))
-        post_list.append(postline)
-    newlink = A('plugin_blog', 'new_post')
+def editing():
     parent = None
     children = None
     docs = None
-    return dict(post_list=post_list,
-                parent=parent,
-                docs=docs,
-                children=children,
-                newlink=newlink)
-
-
-@auth.requires(auth.user_id == 2)
-def new_post():
-    form = crud.create(db.articles)
-    parent = None
-    children = None
-    docs = None
-    return dict(form=form, parent=parent, docs=docs, children=children)
-
-
-@auth.requires(auth.user_id == 2)
-def edit_post():
-    id = request.args[0]
-    form = crud.update(db.articles, id)
-    parent = None
-    children = None
-    docs = None
-    return dict(id=id,
-                form=form,
-                parent=parent,
+    return dict(parent=parent,
                 docs=docs,
                 children=children)
 
 
-@auth.requires(auth.user_id == 2)
-def new_tag():
-    form = crud.create(db.blog_tags)
-    return dict(form=form)
+def topic():
+    '''
+    List articles tagged with a particular blog_tags item.
 
+    The first url argument should be the topic to be displayed, and can be
+    either the row id number (int) or the tag name (string). If no argument
+    is specified, provide an alphabetical list of links to all tags.
+    '''
+    try:
+        # Is the argument an id or a name string?
+        the_arg = request.args[0]
+        if re.match(r'[1-9]+', the_arg):
+            the_id = int(the_arg)
+        else:
+            the_id = db(db.blog_tags.tagname == the_arg).select().first().id
 
-@auth.requires(auth.user_id == 2)
-def new_doc():
-    form = crud.create(db.docs)
-    parent = None
-    children = None
-    docs = None
-    return dict(form=form, parent=parent, docs=docs, children=children)
+        tag_arts = db((db.articles.id == the_id) &
+                      (db.articles.blog_tags.contains(the_id))
+                      ).select(orderby=~db.articles.created)
+        for a in tag_arts:
+            if a.teaser == '':
+                a.teaser = smart_truncate(a.body)
+            a.author = '{} {}'.format(db.auth_user[a.author].first_name,
+                                    db.auth_user[a.author].last_name)
+        taglist = None
+
+    except IndexError:
+        tag_arts = None
+        taglist = db().select(db.blog_tags.ALL)
+
+    return dict(articles=tag_arts, taglist=taglist)
--- routes.py
+++ routes.py
@@ -13,11 +13,9 @@ routes_in = (
     ('/login', '/blog/default/user/login'),
     ('/blog/default/index', '/blog/plugin_blog/index'),
     ('/articles/$anything', '/blog/plugin_blog/articles/$anything'),
+    ('/topic/$anything', '/blog/plugin_blog/topic/$anything'),
     ('/classes/$anything', '/blog/plugin_blog/classes/$anything'),
-    ('/new_post', '/blog/plugin_blog/new_post'),
-    ('/edit_post/$anything', '/blog/plugin_blog/edit_post/$anything'),
-    ('/all_posts', '/blog/plugin_blog/all_posts'),
-    ('/new_doc', '/blog/plugin_blog/new_doc'),
+    ('/editing/$anything', '/blog/plugin_blog/editing/$anything'),
     # don't break download links
     ('/blog/default/download/$anything', '/blog/default/download/$anything'),
     # make sure you do not break admin
@@ -30,11 +28,9 @@ routes_in = (
 routes_out = (
     ('/blog/plugin_blog/index', '/'),
     ('/blog/plugin_blog/articles/$anything', '/articles/$anything'),
+    ('/blog/plugin_blog/topic/$anything', '/topic/$anything'),
     ('/blog/plugin_blog/classes/$anything', '/classes/$anything'),
-    ('/blog/plugin_blog/new_post', '/new_post'),
-    ('/blog/plugin_blog/edit_post/$anything', '/edit_post/$anything'),
-    ('/blog/plugin_blog/all_posts', '/all_posts'),
-    ('/blog/plugin_blog/new_doc', '/new_doc'),
+    ('/blog/plugin_blog/editing/$anything', '/editing/$anything'),
     # don't break download links
     ('/blog/default/download/$anything', '/blog/default/download/$anything'),
     # make sure you do not break admin
