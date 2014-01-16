1b20e78ad9680a9924e03bea4880036e7bc66136
Ian W. Scott <iscott@tyndale.ca>
2014-01-15 22:21:15 -0500

    Recovered working version of witnesses code

--- plugins/plugin_woh/controllers/plugin_woh.py
+++ plugins/plugin_woh/controllers/plugin_woh.py
@@ -2,40 +2,62 @@
 # -*- coding:utf-8 -*-
 
 if 0:
-    from gluon import SPAN
-from os import path
-import csv
-import re
+    from gluon import SPAN, request, db, H1, H2
 from pprint import pprint
 import traceback
-import datetime
+
 
 def section():
     try:
-        print 'starting section'
         chapnum = request.args[0] if request.args[0] else 0
         secnum = request.args[1] if request.args[0] else 0
-        print 'chapter', chapnum
-        print 'section', secnum
         pars = db((db.paragraphs.chapter == chapnum) &
-                  (db.paragraphs.section == secnum)).select(orderby = db.paragraphs.subsection)
-        print 'found {} paragraphs'.format(len(pars))
-        txt = DIV()
+                  (db.paragraphs.section == secnum)
+                  ).select(orderby=db.paragraphs.subsection)
+        print 'found {} par rows'.format(len(pars))
+        paragraphs = []
         for p in pars:
-            num = SPAN('.'.join([str(chapnum), str(secnum), str(p.subsection)]),
-                    _class='woh-refnum')
-            myp = P(num, p.body)
-            txt.append(myp)
-        return {'text': txt}
+            mypar = {}
+            num = SPAN('.'.join([s for s in [str(chapnum), str(secnum),
+                                             str(p.subsection)] if s]),
+                       _class='woh-refnum')
+            mypar['num'] = num
+            mypar['text'] = p.body
+            if p.audio:
+                for a in p.audio:
+                    mypar['auds'] = []
+                    arow = db.woh_audio(a)
+                    mypar['auds'].append((arow.audio_title,
+                                          arow.audio_description,
+                                          arow.audio_file_mp3,
+                                          arow.audio_file_ogg))
+            else:
+                mypar['auds'] = None
+
+            if p.image:
+                for i in p.image:
+                    mypar['images'] = []
+                    irow = db.woh_images(i)
+                    mypar['images'].append((irow.img_title,
+                                            irow.image_description,
+                                            irow.img_file))
+            else:
+                mypar['images'] = None
+
+            paragraphs.append(mypar)
+
+        pprint(paragraphs)
+        return {'paragraphs': paragraphs}
     except Exception:
         print traceback.format_exc(5)
+
 
 def read():
     chapnum = request.args[0] if request.args[0] else 0
     secnum = request.args[1] if request.args[1] else 1
     title1 = H1(db.chapter_titles(db.chapter_titles.num == chapnum).title)
     sec = db((db.section_titles.chapter_num == chapnum) &
-                   (db.section_titles.section_num == secnum)).select().first()
+             (db.section_titles.section_num == secnum)).select().first()
     try:
         title2 = H2(sec.title)
     except AttributeError:
@@ -43,82 +65,3 @@ def read():
     print 'reading'
     return {'title1': title1, 'title2': title2,
             'chapnum': chapnum, 'secnum': secnum}
-
-def woh_import():
-    try:
-        db.paragraphs.truncate()
-    except:
-        print traceback.format_exc(5)
-    mydir = '/home/ian/Dropbox/Downloads/Webdev/woh_export'
-    files = [
-             'node-export(43-nodes).1335558252.csv',
-             'node-export(50-nodes).1335557844.csv',
-             'node-export(50-nodes).1335557890.csv',
-             'node-export(50-nodes).1335557934.csv',
-             'node-export(50-nodes).1335558015.csv',
-             'node-export(50-nodes).1335558056.csv',
-             'node-export(50-nodes).1335558115.csv',
-             'node-export(50-nodes).1335558151.csv',
-             'node-export(50-nodes).1335558186.csv'
-             ]
-    #'node-export[](1-nodes).1335557290.export',
-    fullfiles = [path.join(mydir, f) for f in files]
-
-    for ff in fullfiles:
-        with open(ff, 'rU') as csfile:
-            rows = csv.DictReader(csfile)
-            for row in rows:
-                #pprint(row)
-                titlebits = row['title'].split('.') if row['title'] else [None, None, None]
-                taxes = [v for k, v in row.iteritems()
-                         if re.match(r'.*taxonomy.*', k)
-                         and v not in ['NULL', '', 0, 'None', None]]
-                topics = '|'.join(taxes) if taxes else None
-                pullquote = row['field_pullquote[\'0\'][\'value\']'] \
-                        if 'field_pullquote[\'0\'][\'value\']' in row.values() \
-                        else None
-                audio = row['field_audiolink[\'0\'][\'value\']'] \
-                        if 'field_audiolink[\'0\'][\'value\']' in row.values() \
-                        else None
-                image_id = row['field_images[\'0\'][\'fid\']'] \
-                        if 'field_images[\'0\'][\'fid\']' in row.values() \
-                        else None
-                image_alt = row['field_images[\'0\'][\'data\'][\'alt\']'] \
-                        if 'field_images[\'0\'][\'data\'][\'alt\']' \
-                        in row.values() else None
-                image_title = row['field_images[\'0\'][\'data\'][\'title\']'] \
-                        if 'field_images[\'0\'][\'data\'][\'title\']' \
-                        in row.values() else None
-                image_filename = row['field_images[\'0\'][\'filename\']'] \
-                        if 'field_images[\'0\'][\'filename\']' in row.values() \
-                        else None
-                times = {}
-                for k in ['changed', 'created']:
-                    errors = 0
-                    try:
-                        times[k] = datetime.datetime.fromtimestamp(int(row[k]))
-                    except (TypeError, ValueError):
-                        times[k] = datetime.datetime.utcnow()
-                        errors += 1
-                    print '{} errors: {}'.format(k, errors)
-
-                matches = {'uid': row['uid'],
-                           'chapter': titlebits[0],
-                           'section': titlebits[1] if len(titlebits) > 1 else 0,
-                           'subsection': titlebits[2] if len(titlebits) > 1 else 0,
-                           'display_title': row['field_displaytitle[\'0\'][\'value\']'],
-                           'status': row['status'],
-                           'changed': times['changed'],
-                           'created': times['created'],
-                           'body': row['body'],
-                           'pullquote': pullquote,
-                           'audio': audio,
-                           'image_id': image_id,
-                           'image_alt': image_alt,
-                           'image_title': image_title,
-                           'image_filename': image_filename,
-                           'topics': topics}
-                matches = {k: v for k, v in matches.iteritems() if not v in [None, 'NULL']}
-                num = db.paragraphs.insert(**matches)
-                print num
-
--- plugins/plugin_woh/models/plugin_woh.py
+++ plugins/plugin_woh/models/plugin_woh.py
@@ -1,39 +1,62 @@
 #! /usr/bin/python
 # -*- coding:utf-8 -*-
 
+if 0:
+    from gluon import Field, db, request
 import datetime
+import os
+now = datetime.datetime.utcnow()
 
 db.define_table('chapter_titles',
-                Field('num', 'integer'),
-                Field('title', 'string'),
+                Field('num'),
+                Field('title'),
                 format=lambda r: '{}: {}'.format(r.num, r.title)
                 )
 
-
 db.define_table('section_titles',
-                Field('chapter_num', 'integer'),
-                Field('section_num', 'integer'),
+                Field('chapter_num'),
+                Field('section_num'),
                 Field('title', 'string'),
                 format=lambda r: '{}.{}: {}'.format(r.chapter_num,
                                                     r.section_num, r.title)
                 )
 
+db.define_table('woh_audio',
+                Field('audio_title'),
+                Field('audio_description'),
+                Field('audio_file_mp3', 'upload',
+                      uploadfolder=os.path.join(request.folder,
+                                                'static/audio')),
+                Field('audio_file_ogg', 'upload',
+                      uploadfolder=os.path.join(request.folder,
+                                                'static/audio')),
+                format=lambda r: r.audio_title
+                )
+
+db.define_table('woh_images',
+                Field('image_title'),
+                Field('image_description'),
+                Field('img_file', 'upload',
+                      uploadfolder=os.path.join(request.folder,
+                                                'static/images')),
+                format=lambda r: r.audio_title
+                )
+
 db.define_table('paragraphs',
                 Field('uid', 'integer'),
                 Field('chapter', 'string'),
+                Field('chapter_id', 'reference chapter_titles'),
                 Field('section', 'string'),
+                Field('section_id', 'reference section_titles'),
                 Field('subsection', 'string'),
                 Field('display_title'),
                 Field('status', 'string'),
-                Field('created'), #'datetime', default = datetime.datetime.utcnow()
-                Field('changed'),
+                Field('created'),  # 'datetime', default = now
+                Field('changed'),  # 'datetime', default = now
                 Field('body', 'text'),
                 Field('pullquote'),
-                Field('audio', 'integer'),
-                Field('image_id', 'integer'),
-                Field('image_alt'),
-                Field('image_title'),
-                Field('image_filename'),
-                Field('topics')
+                Field('audio', 'list:reference woh_audio'),
+                Field('image', 'list:reference woh_images'),
+                Field('topics'),
+                format=lambda r: '{}, {}, {}'.format(r.chapter, r.section, r.subsection)
                 )
-
