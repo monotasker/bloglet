0f40c1f4a83b615959ccb799b39b1af784b00d1d
Ian Scott <scottianw@gmail.com>
2014-02-20 15:38:31 -0500

    Restored proper version of plugin_ajaxselect (fixing error)

--- models/plugin_blog.py
+++ models/plugin_blog.py
@@ -1,12 +1,19 @@
+#! /usr/bin/python
+# -*-coding:utf8-*-
+
 import datetime
 import os
+from plugin_ajaxselect import AjaxSelect
 
 if 0:
-    from gluon import current, IS_IN_DB, Field, IS_EMPTY_OR
+    from gluon import current, IS_IN_DB, Field, IS_EMPTY_OR, URL
     db = current.db
     auth = current.auth
     request = current.request
+    response = current.response
 
+response.files.insert(5, URL('static',
+                      'plugin_ajaxselect/plugin_ajaxselect.js'))
 
 db.define_table('blog_images',
                 Field('uid', 'integer'),
@@ -50,6 +57,20 @@ db.articles.parent.requires = IS_EMPTY_OR(IS_IN_DB(db, 'articles.id',
 db.articles.blog_tags.requires = IS_IN_DB(db, 'blog_tags.id',
                                           db.blog_tags._format,
                                           multiple=True)
+db.articles.blog_tags.widget = lambda field, value: \
+                                AjaxSelect(field, value,
+                                            indx=1,
+                                            refresher=True,
+                                            multi='basic',
+                                            lister='simple',
+                                            orderby='tagname').widget()
 db.articles.docs.requires = IS_EMPTY_OR(IS_IN_DB(db, 'docs.id',
                                                  db.docs._format,
                                                  multiple=True))
+db.articles.docs.widget = lambda field, value: \
+                          AjaxSelect(field, value,
+                                     indx=2,
+                                     refresher=True,
+                                     multi='basic',
+                                     lister='simple',
+                                     orderby='label').widget()
